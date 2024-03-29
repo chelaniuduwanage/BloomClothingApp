528cb3a74c384694b638aacb19663f8b3f1bf9f5
chelaniuduwanage <chelanihansika25@gmail.com>
2024-03-29 22:13:38 +0530

    Initial Commit

---
+++ BloomClothingApp/BloomClothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomClothingAppApp.swift
+//  BloomClothingApp
+//
+//  Created by User on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct BloomClothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
