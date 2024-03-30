627ea9706c45e431e69317c2a11f358fa8808231
chelaniuduwanage <chelanihansika25@gmail.com>
2024-03-30 23:45:12 +0530

    payment button and image swap view

---
+++ BloomClothingApp/Views/ImageSwapView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSwapView.swift
+//  BloomClothingApp
+//
+//  Created by User on 2024-03-30.
+//
+
+import SwiftUI
+
+struct ImageSwapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageSwapView()
+}
---
+++ BloomClothingApp/Views/PaymentButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentButtonView.swift
+//  BloomClothingApp
+//
+//  Created by User on 2024-03-30.
+//
+
+import SwiftUI
+
+struct PaymentButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentButtonView()
+}
